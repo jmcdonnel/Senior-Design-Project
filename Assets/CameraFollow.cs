cf4b7ffc0c37fc0be494675f2743b4d20456365a
Jack McDonnell <jmcdonne@uwyo.edu>
2021-01-31 00:23:38 +0000

    Added camera follow player.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour   
+{
+    public Camera sceneCamera;
+    private Transform playerPos;
+    private Vector3 mousePosition;
+    private double mouseCalc;
+
+
+    void Awake()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        
+    }
+
+
+
+        // Update is called once per framea
+    void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 1;
+        //mousePosition = sceneCamera.ScreenToWorldPoint(Input.mousePosition);
+        
+        transform.position = new Vector3(playerPos.position.x + (mousePosition.x/4), playerPos.position.y + (mousePosition.y / 4), transform.position.z);
+
+        Debug.Log(mousePosition.x / 4);
+
+
+    }
+
+
+}
