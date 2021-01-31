2491fa42cc2e534b36d118efc155221d60785f57
Jack McDonnell <jmcdonne@uwyo.edu>
2021-01-31 07:42:30 +0000

    Turning player sprite to look in direction of mouse

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -15,17 +15,95 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 moveDirection;
 
-    private Vector2 mousePosition;
+    private Vector3 mousePosition;
 
+    public SpriteRenderer spriteRenderer;
 
+    public Sprite newSprite;
 
+    [SerializeField] private Sprite[] index;
+    private SpriteRenderer _spriteRenderer;
 
+    private float mouseCalc;
+    private float mouseCalcY;
+    private Transform playerPos;
+    private Vector3 playerPosition;
+
+    private void Start()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+        playerPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+
+    }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInputs();
-        
+        {
+            
+
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 1;
+             
+            if(mousePosition.x - playerPos.position.x >= 0 && mousePosition.y - playerPos.position.y >= 0) 
+            {
+                
+                if (_spriteRenderer.sprite == index[1])
+                {
+                    _spriteRenderer.flipX = false;
+                }else if (_spriteRenderer.sprite == index[0])
+                {
+                    _spriteRenderer.sprite = index[1];
+                }
+                
+            }
+            
+            else if (mousePosition.x - playerPos.position.x < 0 && mousePosition.y - playerPos.position.y > 0)
+            {
+                if (_spriteRenderer.sprite == index[1] )
+                {
+                    _spriteRenderer.flipX = true;
+
+                }
+                else if (_spriteRenderer.sprite == index[0])
+                {
+                    _spriteRenderer.sprite = index[1];
+                }
+
+            }
+            else if (mousePosition.x - playerPos.position.x < 0 && mousePosition.y - playerPos.position.y  < 0)
+            {
+                if (_spriteRenderer.sprite == index[1])
+                {
+                    _spriteRenderer.sprite = index[0];
+                    
+
+                }
+                else if (_spriteRenderer.sprite == index[0])
+                {
+                    _spriteRenderer.flipX = true;
+                }
+
+            }
+            else if (mousePosition.x - playerPos.position.x >= 0 && mousePosition.y - playerPos.position.y <= 0)
+            {
+                if (_spriteRenderer.sprite == index[1])
+                {
+                    _spriteRenderer.sprite = index[0];
+
+
+                }
+                else if (_spriteRenderer.sprite == index[0])
+                {
+                    _spriteRenderer.flipX = false;
+                }
+
+            }
+
+        }
+
     }
     void FixedUpdate()
     {
@@ -52,12 +130,12 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
 
-        // Rotate weapon to follow mouse
-        Vector2 aimDirection = mousePosition - rb.position;
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
-        rb.rotation = aimAngle;
         
   
 
     }
+
+    //                         O7
+    //private float pythagoreanTheorem(float x, float y)
+
 }
